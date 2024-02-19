26107cb3c6375be69c07255431f77fa40c2d11df
Crowdbotics <team@crowdbotics.com>
2024-02-19 05:15:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-dream-46460",
+  url: "https://polished-dream-46460.botics.co",
+  api: "https://polished-dream-46460.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
